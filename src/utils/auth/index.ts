025ae7f1563e0982b7a54ea89e06cd792eb6428e
IvanSeagull <47693465+IvanSeagull@users.noreply.github.com>
2023-03-14 14:32:55 +0000

    feat: user register

--- src/utils/auth/index.ts
+++ src/utils/auth/index.ts
@@ -11,3 +11,15 @@ export const authUser = async (wallet: string, cb = (res: any) => console.log(re
     .then((res) => res.json())
     .then((res) => cb(res));
 };
+
+export const registerUser = async (body: any, cb = (res: any) => console.log(res)) => {
+  fetch(`${apiUrl}/users/register`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(body),
+  })
+    .then((res) => res.json())
+    .then((res) => res.success && cb(res));
+};
